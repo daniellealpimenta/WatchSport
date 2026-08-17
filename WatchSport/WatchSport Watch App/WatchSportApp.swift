4d921f98ad87264019b75d012ba24c1cbff2fbbd
Daniel Leal Pimenta <129066504+daniellealpimenta@users.noreply.github.com>
2026-08-17 15:03:40 -0300

    feat: Adding the base of the project

---
+++ WatchSport/WatchSport Watch App/WatchSportApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchSportApp.swift
+//  WatchSport Watch App
+//
+//  Created by Daniel Leal PImenta on 17/08/26.
+//
+
+import SwiftUI
+
+@main
+struct WatchSport_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
